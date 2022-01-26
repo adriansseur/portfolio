4980dc9dda4f4d491a340fd5c0794bd991908d54
Adrian Lugo <misteralugo@gmail.com>
2022-01-25 23:25:54 -0600

    static kitchen image

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react"
+import kitchen from "../images/kitchen.png"
 
 export default function About() {
     return (
         <div className="about-container">
             <h1>About</h1>
             <div className="about-content">
-                <div className="left">Image</div>
+                <div className="left">
+                    <img className="kitchen" src={kitchen} alt="a technological kitchen"></img>
+                </div>
                 <div className="right">
                     <p>I am highly motivated to work on building things for the web. I enjoy working with others, learning through executing, and taking responsibility for my projects. I like science, coffee, and chocolate chip cookies way too much.</p>
                     <div className="resume-container">
