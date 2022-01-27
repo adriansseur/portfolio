3536d1b956c491d6c929404cbdd91e01f72f4781
Adrian Lugo <misteralugo@gmail.com>
2022-01-26 18:56:28 -0600

    basic form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,18 @@ export default function Contact() {
         <div className="contact-container" id="contact-container">
             <h1>Contact</h1>
             <div className="contact-content">
-                <div className="form">Form</div>
-                <div className="email">Email</div>
-                <div className="image">Image</div>
+                <div className="form">
+                    
+                    <form action="https://formsubmit.co/misteralugo@gmail.com" method="POST">
+                        <input type="hidden" name="_subject" value="New Email From Porfolio!"></input>
+                        <input type="text" name="name" required placeholder="Name"></input>
+                        <input type="email" name="email" required placeholder="Email Address"></input>
+                        <input type="text" name="message" required placeholder="Message"></input>
+                        <input type="hidden" name="_next" value="https://adriansseur.github.io"></input>
+                        <button type="submit">Submit</button>
+                    </form>
+
+                </div>
             </div>
         </div>
     )
