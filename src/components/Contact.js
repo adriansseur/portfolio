df5b6e5621d9e51c9807fcc4e0f5b6a9764661fc
Adrian Lugo <misteralugo@gmail.com>
2022-01-27 21:34:35 -0600

    basic contact drawing animation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,12 @@
 import React from "react"
+import blueBook from "../images/blueBook.png"
+import booksCompu from "../images/booksCompu.png"
+import devBack from "../images/devBack.png"
+import leftArm from "../images/leftArm.png"
+import newMessage from "../images/newMessage.png"
+import redDot from "../images/redDot.png"
+import rightArm from "../images/rightArm.png"
+import yellowBook from "../images/yellowBook.png"
 
 export default function Contact() {
     return (
@@ -17,6 +25,16 @@ export default function Contact() {
                     </form>
 
                 </div>
+            </div>
+            <div className="images-container">
+                <img src={booksCompu} alt="a bookshelf and a computer desk"></img>
+                <img className="new-message" src={newMessage} alt="a new message on the screen"></img>
+                <img src={redDot} alt="a red dot indicating a new message"></img>
+                <img className="blue-book" src={blueBook} alt="a blue book"></img>
+                <img className="yellow-book" src={yellowBook} alt="a yellow book"></img>
+                <img className="left-arm" src={leftArm} alt="a left arm"></img>
+                <img className="right-arm" src={rightArm} alt="a right arm"></img>
+                <img src={devBack} alt="the back of a web developer"></img>
             </div>
         </div>
     )
