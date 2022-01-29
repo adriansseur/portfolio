ad4b6ad81ced1913f346b5dcba7d9ae941cae0d8
Adrian Lugo <misteralugo@gmail.com>
2022-01-28 21:02:53 -0600

    contact resize with keyboard spacing trick

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,6 @@ export default function Contact() {
             <h1>Contact</h1>
             <div className="contact-content">
                 <div className="form">
-                    
                     <form action="https://formsubmit.co/misteralugo@gmail.com" method="POST">
                         <input type="hidden" name="_subject" value="New Email From Porfolio!"></input>
                         <input type="text" name="name" required placeholder="Name"></input>
@@ -23,18 +22,17 @@ export default function Contact() {
                         <input type="hidden" name="_next" value="https://adriansseur.github.io"></input>
                         <button type="submit">Submit</button>
                     </form>
-
                 </div>
-            </div>
-            <div className="images-container">
-                <img src={booksCompu} alt="a bookshelf and a computer desk"></img>
-                <img className="new-message" src={newMessage} alt="a new message on the screen"></img>
-                <img src={redDot} alt="a red dot indicating a new message"></img>
-                <img className="blue-book" src={blueBook} alt="a blue book"></img>
-                <img className="yellow-book" src={yellowBook} alt="a yellow book"></img>
-                <img className="left-arm" src={leftArm} alt="a left arm"></img>
-                <img className="right-arm" src={rightArm} alt="a right arm"></img>
-                <img src={devBack} alt="the back of a web developer"></img>
+                <div className="images-container">
+                    <img src={booksCompu} alt="a bookshelf and a computer desk"></img>
+                    <img className="new-message" src={newMessage} alt="a new message on the screen"></img>
+                    <img src={redDot} alt="a red dot indicating a new message"></img>
+                    <img className="blue-book" src={blueBook} alt="a blue book"></img>
+                    <img className="yellow-book" src={yellowBook} alt="a yellow book"></img>
+                    <img className="left-arm" src={leftArm} alt="a left arm"></img>
+                    <img className="right-arm" src={rightArm} alt="a right arm"></img>
+                    <img src={devBack} alt="the back of a web developer"></img>
+                </div>
             </div>
         </div>
     )
