6bb34daf66a43d44928da2fe8021cbae8c010a39
Adrian Lugo <misteralugo@gmail.com>
2022-04-01 13:30:28 -0500

    about grid and hero img improvements

--- src/components/About.js
+++ src/components/About.js
@@ -19,31 +19,29 @@ export default function About() {
                     <br /><br />
                     I am <strong>highly motivated</strong> and <strong>great in a group</strong>. I like science, coffee, and chocolate chip cookies. </p>
                 <a href={resume} download>Resume</a>
-                <div className="highlights-container">
-                    <div className="education">
-                        <p>Relevant Education</p>
-                        <ul>
-                            <li>Scrimba's Front-End Developer Career Path</li>
-                            <li>Bachelor's Degree</li>
-                        </ul>
-                    </div>
-                    <div className="tech-stack">
-                        <p>Tech Stack</p>
-                        <ul className="tech-stack-grid">
-                            <li>React</li>
-                            <li>Javascript</li>
-                            <li>CSS</li>
-                            <li>HTML</li>
-                            <li>Git</li>
-                            <li>APIs</li>
-                            <li>Responsive</li>
-                            <li>UI Design</li>
-                            <li>Typescript</li>
-                            <li>Sass</li>
-                            <li>GSAP</li>
-                            <li>Barba</li>
-                        </ul>
-                    </div>
+                <div className="education">
+                    <p>Relevant Education</p>
+                    <ul>
+                        <li>Scrimba's Front-End Developer Career Path</li>
+                        <li>Bachelor's Degree</li>
+                    </ul>
+                </div>
+                <div className="tech-stack">
+                    <p>Tech Stack</p>
+                    <ul className="tech-stack-grid">
+                        <li>React</li>
+                        <li>Javascript</li>
+                        <li>CSS</li>
+                        <li>HTML</li>
+                        <li>Git</li>
+                        <li>APIs</li>
+                        <li>Responsive</li>
+                        <li>UI Design</li>
+                        <li>Typescript</li>
+                        <li>Sass</li>
+                        <li>GSAP</li>
+                        <li>Barba</li>
+                    </ul>
                 </div>
                 <div className="images-container">
                     <img className="kitchen" src={kitchen} alt="a technological kitchen"></img>
